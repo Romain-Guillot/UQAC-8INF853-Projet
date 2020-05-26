84acf70770f61df272f37a88dd480710f1311457
Romain Guillot <rguillot42@gmail.com>
2020-05-26 15:31:35 -0400

    Split struts conf files into multiple files for each module

--- StableManager/src/main/java/com/uqac/stablemanager/auth/service/AuthenticationService.java
+++ StableManager/src/main/java/com/uqac/stablemanager/auth/service/AuthenticationService.java
@@ -4,13 +4,10 @@ import com.uqac.stablemanager.member.model.MemberModel;
 import org.apache.struts2.ServletActionContext;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 
-import java.util.Collection;
 
 public class AuthenticationService {
 
