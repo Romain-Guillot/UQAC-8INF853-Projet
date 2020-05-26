4a18eaf95529a4df90eb1b43ee7b4a3ab27c4406
Romain Guillot <rguillot42@gmail.com>
2020-05-26 15:20:28 -0400

    (stable) add login / logout features with redirections and access control

--- StableManager/src/main/java/com/uqac/stablemanager/auth/action/LoginAction.java
+++ StableManager/src/main/java/com/uqac/stablemanager/auth/action/LoginAction.java
@@ -2,6 +2,7 @@ package com.uqac.stablemanager.auth.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 import com.uqac.stablemanager.auth.model.CredentialModel;
+import com.uqac.stablemanager.auth.service.AuthenticationService;
 
 public class LoginAction extends ActionSupport {
 
@@ -9,7 +10,9 @@ public class LoginAction extends ActionSupport {
 
     @Override
     public String execute() throws Exception {
+        System.err.println("LOGIN ACTION");
         credential = new CredentialModel();
+        new AuthenticationService().login("", "");
         return SUCCESS;
     }
 }
--- StableManager/src/main/java/com/uqac/stablemanager/auth/service/AuthenticationService.java
+++ StableManager/src/main/java/com/uqac/stablemanager/auth/service/AuthenticationService.java
@@ -1,13 +1,66 @@
 package com.uqac.stablemanager.auth.service;
 
+import org.apache.struts2.ServletActionContext;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+
+import java.util.Collection;
+
 public class AuthenticationService {
 
+    public class Profile implements UserDetails {
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return null;
+        }
+
+        @Override
+        public String getPassword() {
+            return null;
+        }
+
+        @Override
+        public String getUsername() {
+            return null;
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return false;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return false;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return false;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return false;
+        }
+    }
 
     public void login(String email, String password) {
-
+        UserDetails user = new Profile();
+        Authentication auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(auth);
     }
 
     public void logout() {
-
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(ServletActionContext.getRequest(), ServletActionContext.getResponse(), auth);
+        }
+        SecurityContextHolder.getContext().setAuthentication(null);
     }
 }
