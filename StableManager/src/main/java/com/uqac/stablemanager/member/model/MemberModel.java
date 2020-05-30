077edaf7e933fedc1e461e174ccd393dfa455531
Romain Guillot <rguillot42@gmail.com>
2020-05-30 12:19:48 -0400

    create database method helper

--- StableManager/src/main/java/com/uqac/stablemanager/member/model/MemberModel.java
+++ StableManager/src/main/java/com/uqac/stablemanager/member/model/MemberModel.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
 
 public class MemberModel implements UserDetails {
@@ -34,7 +35,7 @@ public class MemberModel implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return role.getRights();
+        return role == null ? Collections.emptyList() : role.getRights();
     }
 
     public void setId(int id) {
